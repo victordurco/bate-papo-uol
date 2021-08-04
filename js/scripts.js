12ec44fe28baf9c247925317ec6a68242383ef87
victordurco <victordurco@gmail.com>
2021-08-04 13:48:46 -0300

    login page layout done

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,14 @@
+function enterChat(){
+    let userLogin = document.querySelector(".userLogin");
+    let userName = document.getElementById("userName").value;
+    let submitButton = document.getElementById("submitName");
+
+    if(userName === ""){
+        return;
+    }else{
+        userLogin.style.display = "none";
+    }
+}
 
 function userNav(){
     let nav = document.querySelector("nav");
