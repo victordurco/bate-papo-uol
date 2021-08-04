b79359b97c8b3e6d4314f0f45e31be65158e631f
victordurco <victordurco@gmail.com>
2021-08-04 20:15:39 -0300

    login with a available name done

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,43 @@
-function enterChat(){
-    let userLogin = document.querySelector(".userLogin");
-    let userName = document.getElementById("userName").value;
-    let submitButton = document.getElementById("submitName");
+const msgs = [{name: "Victor", dest:"Todos", time:"12:37:50", msg:"Que site mais bacana"}, {name: "Pedro", dest:"Todos", time:"12:38:50", msg:"It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution"}];
 
-    if(userName === ""){
-        return;
-    }else{
-        userLogin.style.display = "none";
+let userName ="";
+
+function loginName(name){
+    const request = axios.post("https://mock-api.bootcamp.respondeai.com.br/api/v3/uol/participants",{name: name});
+    request.then(nameAvailable);
+    request.catch(nameUnavailable);
+}
+
+function nameAvailable(response){
+    let userLogin = document.querySelector(".userLogin");
+    let name = document.getElementById("userName").value;
+    userName = name;
+    userLogin.style.display = "none";
+}
+
+function nameUnavailable(error){
+    let statusCode = error.response.status;
+    if(statusCode === 400){
+        alert("O nome já está sendo usado ou é inválido");
     }
+}
+
+function loadChat(){
+    let chat = document.querySelector("main");
+    let chatContent = "";
+    for (let i=0; i<msgs.length; i++){
+        chatContent += `
+        <li class="msg">
+           <p>(${msgs[i].time}) <span class="bold">${msgs[i].name}</span> para <span class="bold">${msgs[i].dest}</span>: ${msgs[i].msg}</p> 
+        </li>`
+    }
+    chat.innerHTML = chatContent;
+}
+
+function enterChat(){
+    let userName = document.getElementById("userName").value;
+    loginName(userName);
+    loadChat();
 }
 
 function userNav(){
